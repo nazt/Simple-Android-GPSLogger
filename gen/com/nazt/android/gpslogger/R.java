86bdd1d10f207583ac705909274d857406456b3e
Nat <nat.weerawan@gmail.com>
2010-11-07 02:50:19 +0700

    Rearrange layout

--- gen/com/nazt/android/gpslogger/R.java
+++ gen/com/nazt/android/gpslogger/R.java
@@ -15,14 +15,13 @@ public final class R {
         public static final int gpslogger48=0x7f020001;
     }
     public static final class id {
-        public static final int ButtonNewTrip=0x7f050005;
-        public static final int ButtonStart=0x7f050001;
-        public static final int ButtonStop=0x7f050002;
-        public static final int EditTextTripName=0x7f050004;
+        public static final int ButtonStart=0x7f050003;
+        public static final int ButtonStop=0x7f050004;
+        public static final int EditTextTripName=0x7f050002;
         public static final int ScrollView01=0x7f050000;
-        public static final int TableLayout01=0x7f050006;
-        public static final int TableRow01=0x7f050007;
-        public static final int TextViewCurrentTripName=0x7f050003;
+        public static final int TableLayout01=0x7f050005;
+        public static final int TableRow01=0x7f050006;
+        public static final int TextViewCurrentTripName=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
